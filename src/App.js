eb39049571e1eabdbe0870c513433db749febe2a
fredkamm <fred.kamm95@gmail.com>
2023-01-11 11:38:29 -0500

    routing is set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import React, { Suspense } from "react";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import CreatePost from "./pages/CreatePost";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Router>
+      <Suspense fallback={<p>Loading... </p>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/createpost" element={<CreatePost />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/CreatePost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreatePost() {
+  return (
+    <div>createPost</div>
+  )
+}
+
+export default CreatePost
