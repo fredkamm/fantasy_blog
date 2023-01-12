3a7f6de7ed6d81c9fb5c65602b5e9e985237cd04
fredkamm <fred.kamm95@gmail.com>
2023-01-12 18:45:10 -0500

    pages implement, fixing some bugs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React, { Suspense } from "react";
+import React, { useState } from "react";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
+// import Navbar from "./pages/navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import CreatePost from "./pages/CreatePost";
 
+import { auth } from "@firebase/auth";
+import { signOut } from "firebase/auth";
+
 function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
+  const signOutUser = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+    });
+  };
+
   return (
     <Router>
-      <Suspense fallback={<p>Loading... </p>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/createpost" element={<CreatePost />} />
-        </Routes>
-      </Suspense>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/createpost">Create Post</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home isAuth={isAuth} />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+        <Route path="/createpost" element={<CreatePost isAuth={isAuth} />} />
+      </Routes>
     </Router>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import GoogleButton from "react-google-button";
 
-function Login() {
-  return <div>login</div>;
+import { auth, provider } from "../config/firebase";
+import { signInWithRedirect } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+function Login({ setIsAuth }) {
+  let navigate = useNavigate();
+  const googleSignIn = () => {
+    signInWithRedirect(auth, provider).then((results) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/")
+    });
+  };
+
+  return (
+    <div className="loginPage">
+      <p>Sign in</p>
+      <GoogleButton onClick={googleSignIn} />
+    </div>
+  );
 }
 
 export default Login;
