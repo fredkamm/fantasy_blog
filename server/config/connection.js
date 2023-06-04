a78d8cc51e678879d389f47a97af729828ac15b0
fredkamm <fred.kamm95@gmail.com>
2023-06-04 10:31:00 -0400

    sucessful heroku deployement

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fantasyblog', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fantasyblog');
 
-const db = mongoose.connection;
-
-db.on('error', (error) => {
-  console.error('MongoDB connection error:', error);
-});
-
-module.exports = db;
+module.exports = mongoose.connection;
